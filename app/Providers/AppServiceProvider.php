b6dd90819a7735f3081599f7185aff32a7bd09a6
JP Caparas <jp@jpcaparas.com>
2025-01-12 22:06:10 +1300

    feat: add RestaurantChatStateProcessor and update AppServiceProvider for state handling

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\State\RestaurantChatStateProcessor;
 use ApiPlatform\State\ProcessorInterface;
 use App\State\RestaurantSearchStateProcessor;
 use Illuminate\Support\ServiceProvider;
@@ -13,7 +14,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->tag([RestaurantSearchStateProcessor::class], ProcessorInterface::class);
     }
 
     /**
@@ -21,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->app->tag([RestaurantSearchStateProcessor::class], ProcessorInterface::class);
+		$this->app->tag(RestaurantChatStateProcessor::class, ProcessorInterface::class);
     }
 }
